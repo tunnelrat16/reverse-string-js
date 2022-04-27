8c0ccdd86ea3997c3b031d1ce240d54deea59d71
Kyle Coberly <kyle.coberly@gmail.com>
2022-04-27 17:38:43 -0600

    Switch to JS

---
+++ reverse-string.js
@@ -0,0 +1,3 @@
+export function reverse(value) {
+  return value.split("").reverse().join("");
+}
--- reverse-string.ts
+++
@@ -1,3 +0,0 @@
-export function reverse(value: string): string {
-  return value.split("").reverse().join("");
-}
