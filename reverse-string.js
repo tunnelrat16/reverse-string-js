026ee1a64e67c073e05f3e73736c2808675e9e6a
tunnelrat16 <tjandmatt@hotmail.com>
2022-04-28 08:46:42 -0700

    initial commit

--- reverse-string.js
+++ reverse-string.js
@@ -1,1 +1,7 @@
-export function reverse() {}
+export function reverse(string) {
+    let newString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
+}
